f134ca6bbae3d08d3f15b1755ab986889bc0ad6b
Robert Castro <ra.castro2@uniandes.edu.co>
2024-02-23 01:05:09 -0500

    Pruebas docker

--- api-gateway/gateway.js
+++ api-gateway/gateway.js
@@ -3,12 +3,40 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-// Proxy para redirigir las solicitudes al endpoint de registrar usuario
-app.use('/api/v1/users', createProxyMiddleware({ target: 'http://127.0.0.1:5000', changeOrigin: true }));
-app.use('/ping', createProxyMiddleware({ target: 'http://127.0.0.1:5000', changeOrigin: true }));
+app.use((req, res, next) => {
+  console.log(`Received request for ${req.url}`);
+  next();
+});
 
+app.use('/api/v1/users', createProxyMiddleware({
+  target: 'http://localhost:5000',
+  changeOrigin: true,
+  logLevel: 'debug',
+  onProxyReq: (proxyReq, req, res) => {
+    console.log(`Proxying request for ${req.url} to ${proxyReq.baseUrl}`);
+  },
+  onError: (err, req, res) => {
+    console.log(`Error proxying ${req.url}: ${err.message}`);
+  }
+}));
 
-// Escucha en un puerto específico
+app.use('/ping', createProxyMiddleware({
+  target: 'http://localhost:5000',
+  changeOrigin: true,
+  logLevel: 'debug',
+  onProxyReq: (proxyReq, req, res) => {
+    console.log(`Proxying request for ${req.url} to ${proxyReq.baseUrl}`);
+  },
+  onError: (err, req, res) => {
+    console.log(`Error proxying ${req.url}: ${err.message}`);
+  }
+}));
+
+app.get('/test', (req, res) => {
+  console.log('Test route accessed');
+  res.send('API Gateway is up and running');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`API Gateway running on port ${PORT}`);
